d9f10b2ba5e3c3438271341fec80b1197ad9e6f8
Beefaz <justinas.geryba@gmail.com>
2023-02-23 01:51:50 +0200

    initial commit

---
+++ src/components/ARScene.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-scene
+      embedded
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+      style="height: 100%; width: 100%;"
+  >
+    <a-assets>
+      <a-asset-item id="teapot-model" src="https://dainty-choux-81d08f.netlify.app/public/Card.gltf"></a-asset-item>
+    </a-assets>
+
+    <a-marker preset="custom" type="pattern" url="./assets/marker.patt">
+      <a-entity
+          gltf-model="#teapot-model"
+          scale="0.01 0.01 0.01"
+          position="0 0 0"
+      ></a-entity>
+    </a-marker>
+
+    <a-entity camera></a-entity>
+  </a-scene>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Set the camera position and rotation
+    const camera = document.querySelector("[camera]");
+    camera.setAttribute("position", "0 0 0");
+    camera.setAttribute("rotation", "0 0 0");
+  },
+};
+</script>
